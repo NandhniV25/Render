39a2cffdaa38489583f73a6e336314a6f46df2a8
Nandhni G A <nandhni.25@gmail.com>
2024-04-17 15:00:37 +0700

    print hi

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import pdfplumber
+# import pdfplumber
 
 app = Flask(__name__)
 
@@ -8,12 +8,14 @@ def hello_world():
     return 'Hello, World!'
 
 @app.route('/file')
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
+def print_fn():
+    return "Hi"
+# def extract_text_from_pdf(pdf_path):
+#     text = ""
+#     with pdfplumber.open(pdf_path) as pdf:
+#         for page in pdf.pages:
+#             text += page.extract_text()
+#     return text
 
-text = extract_text_from_pdf("Cox and Kings Ltd vs SAP India Pvt. Ltd. & Anr.pdf")
-print(text)
+# text = extract_text_from_pdf("Cox and Kings Ltd vs SAP India Pvt. Ltd. & Anr.pdf")
+# print(text)
