23448909da8c37e6e41bf13009dc94586a0c2516
Nandhni G A <nandhni.25@gmail.com>
2024-04-17 14:36:45 +0700

    extracting pdf text

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+import pdfplumber
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/file')
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+text = extract_text_from_pdf("Cox and Kings Ltd vs SAP India Pvt. Ltd. & Anr.pdf")
+print(text)
